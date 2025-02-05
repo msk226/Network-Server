345b72569d6e488f530a7ec5cc6575466494c918
msk226 <kminseok5167@gmail.com>
2025-02-05 19:10:44 +0900

    [V1][FEAT] 서버 상태 출력 메시지 작성

--- src/v1/Server.java
+++ src/v1/Server.java
@@ -12,20 +12,28 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("현재 서버가 " + PORT + "번 포트에서 가동중입니다. ");
 
         Socket socket = serverSocket.accept();
+        System.out.println("클라이언트가 연결되었습니다. ");
 
         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
         DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
         while (true) {
             String messageFromClient = inputStream.readUTF();
+
+            System.out.println("클라이언트가 입력한 메시지는 다음과 같습니다: " + messageFromClient);
             if (messageFromClient.equals("exit")) {
                 break;
             }
             String processMessage = messageFromClient + "!!!";
             outputStream.writeUTF(processMessage);
         }
+
+        System.out.println("연결을 종료합니다.");
+
+        // 자원 정리
         inputStream.close();
         outputStream.close();
         socket.close();
